39cf6f7ce56ff6e57442c47003d4a3dad8778f48
MosingiA <ondicho33@gmail.com>
2025-07-16 14:45:29 +0300

    react-hooks-state-and-events-lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All");  
+  const visibleItems = items.filter((item) => {
+    return selectedCategory === "All" 
+    || item.category === selectedCategory;
+  });
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" value={selectedCategory} 
+        onChange={(e) => setSelectedCategory(e.target.value)}>
+          <option value="All">All</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {visibleItems.map((item) => (
+          <Item 
+          key={item.id} 
+          name={item.name} 
+          category={item.category} 
+          />
         ))}
       </ul>
     </div>
